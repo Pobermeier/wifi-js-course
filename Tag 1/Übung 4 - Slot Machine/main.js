677f087401dfb214aebdc8971ebd171b6f183c1d
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-05-19 21:28:37 +0200

    Started implementing slot machine

---
+++ Tag 1/Übung 4 - Slot Machine/main.js
@@ -0,0 +1,8 @@
+const spinButton = document.querySelector('#spin-btn');
+const clickSound = document.querySelector('#click-sound');
+const spinSound = document.querySelector('#spin-sound');
+
+spinButton.addEventListener('click', () => {
+  // clickSound.play();
+  spinSound.play();
+});
