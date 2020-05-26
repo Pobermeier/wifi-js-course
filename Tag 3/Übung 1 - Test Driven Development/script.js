d79bffd911e291789fcbe4f0282c55f2f04ee1a4
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-05-26 13:47:24 +0200

    UPDATE Day 3 - Exercise 1

--- Tag 3/Übung 1 - Test Driven Development/script.js
+++ Tag 3/Übung 1 - Test Driven Development/script.js
@@ -1,10 +1,17 @@
 var testX = 5;
 
 /* addieren */
+// function addieren(num1, num2) {
+//   if (!num2) return parseFloat(num1.toString().replace(',', '.'));
+//   return (
+//     parseFloat(num1.toString().replace(',', '.')) +
+//     parseFloat(num2.toString().replace(',', '.'))
+//   );
+// }
+
 function addieren(num1, num2) {
-  if (!num2) return parseFloat(num1.toString().replace(',', '.'));
+  if (!num2) return +num1.toString().replace(',', '.');
   return (
-    parseFloat(num1.toString().replace(',', '.')) +
-    parseFloat(num2.toString().replace(',', '.'))
+    +num1.toString().replace(',', '.') + +num2.toString().replace(',', '.')
   );
 }
