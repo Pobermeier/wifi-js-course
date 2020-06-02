a9b8f03f0cd736b74c3b57d7fd872d4f329b478c
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-06-02 12:46:01 +0200

    UPDATE Day 4 - Example 2

--- Tag 4/Übung 2 - AJAX-request to Server/server.js
+++ Tag 4/Übung 2 - AJAX-request to Server/server.js
@@ -5,31 +5,6 @@ const lottoGen = require('./lotto.js');
 http
   .createServer(function (request, response) {
     switch (request.url) {
-      case '/':
-      case '/index.html':
-      case '/index.php':
-        response.writeHead(200, { 'Content-Type': 'text/html, charset=utf-8' });
-
-        fs.readFile('static/index.html', function (err, data) {
-          if (!err) {
-            response.end(data);
-          } else {
-            response.writeHead(404);
-            response.end('Error404');
-          }
-        });
-
-        break;
-
-      case '/jquery-3.5.1.min.js':
-      case '/lotto.js':
-        fs.readFile('static/' + request.url, function (err, data) {
-          if (!err) {
-            response.end(data);
-          }
-        });
-        break;
-
       case '/getnumbers':
         response.writeHead(200, {
           'Content-Type': 'application/json, charset=utf-8',
