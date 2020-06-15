fb544ce659c5f54a28b6f83950252f23e8c682d4
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-06-15 11:30:40 +0200

    UPDATE Day 6 - Exercise 1

--- Tag 6/Übung 1 - Promises/main.js
+++ Tag 6/Übung 1 - Promises/main.js
@@ -1,38 +1,38 @@
-const express = require( 'express' );
-const fs = require( 'fs' );
-const bp = require( 'body-parser' );
+const express = require('express');
+// const fs = require('fs');
+const fs = require('fs').promises;
+const bp = require('body-parser');
 const app = express();
-app.listen( 5006, () => {
-    console.log( 'Server gestartet' );
-} );
 
-app.use( bp.urlencoded({extended:false} ));
-app.use( express.static( 'www') );
+app.listen(5006, () => {
+  console.log('Server gestartet');
+});
 
+app.use(bp.urlencoded({ extended: false }));
+app.use(express.static('www'));
 
-app.get( '/daten', (req, res) => {
-    fs.readFile( './data/test.json', (err,content) => {
-        if ( !err ) {
-         res.status(200).end( content );
-        }
+app.get('/daten', (req, res) => {
+  fs.readFile('./data/test.json')
+    .then((content) => {
+      res.status(200).end(content);
     })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-app.post( '/daten', (req, res) => {
-    var neuezahl = Number( req.body.zahl ); // Datentyp?
-    fs.readFile( './data/test.json', (err,content) => {
-        if ( !err ) {
-            var zahlen = JSON.parse( content );
-            zahlen.daten.push( neuezahl );
-            fs.writeFile( './data/test.json', JSON.stringify( zahlen ), (err) => {
-                // Callback-HELL
-                if ( !err ) {
-                    res.status(200).end( 'gespeichert' );
-                }
-            })
-        }
-    }) 
-   
-
-
-})
+app.post('/daten', (req, res) => {
+  var neuezahl = Number(req.body.zahl);
+  fs.readFile('./data/test.json')
+    .then((content) => {
+      const zahlen = JSON.parse(content);
+      zahlen.daten.push(neuezahl);
+      fs.writeFile('./data/test.json', JSON.stringify(zahlen));
+    })
+    .then(() => {
+      res.status(200).end('gespeichert');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
