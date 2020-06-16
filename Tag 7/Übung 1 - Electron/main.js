f79b9dd2689dc13ff6649416b9db1d14b1967e9f
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-06-16 12:27:37 +0200

    UPDATE Day 7 - Exercise 1

---
+++ Tag 7/Übung 1 - Electron/main.js
@@ -0,0 +1,79 @@
+const { app, BrowserWindow, Menu, shell, ipcMain } = require('electron');
+let window;
+
+let menu = [
+  {
+    label: 'My App',
+    submenu: [
+      { label: 'DevTools', role: 'toggleDevTools' },
+      { label: 'Refesh', role: 'reload' },
+      {
+        label: 'News',
+        click: () => {
+          shell.openExternal('http://www.orf.at');
+        },
+      },
+      {
+        label: 'Applikation beenden',
+        click: () => {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
+
+const startApp = () => {
+  console.log('Electron wurde gestartet.');
+  window = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    resizable: true,
+    movable: true,
+    webPreferences: {
+      nodeIntegration: true,
+      devTools: true,
+    },
+    x: 2000,
+    y: 100,
+  });
+  window.loadFile('index.html');
+  Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
+};
+
+app.on('ready', startApp);
+
+//- MacOS
+app.on('active', () => {
+  if (window === null) {
+    starteApplikattion();
+  }
+});
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
+//+ MacOS
+
+const puppeteer = require('puppeteer');
+let newsItems;
+
+const asyncFn = async () => {
+  const url = 'https://orf.at/';
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  newsItems = await page.$$eval('h3', (stories) =>
+    stories.map((headline) => headline.innerText),
+  );
+
+  console.log(newsItems);
+};
+
+asyncFn();
+
+ipcMain.on('newsItems', (event) => {
+  event.reply('newsItems', newsItems);
+});
