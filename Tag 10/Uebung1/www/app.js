9d7ad57fc886acea92d31fba07261200ab89412e
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-06-30 10:54:31 +0200

    UPDATE Day 10 - Exercise 1

--- Tag 10/Uebung1/www/app.js
+++ Tag 10/Uebung1/www/app.js
@@ -1,35 +1,24 @@
-var starteApp = () => {
+(function () {
+  if (APP) {
+    document.addEventListener('deviceready', init);
+  } else {
+    document.addEventListener('DOMContentLoaded', init);
+  }
 
-    // Canvas FullScreen
-    $( '#imagefield' ).attr({
-        width:$(window).width(),
-        height:$(window).height()
-    }).on( 'touchstart', e => {
-        paintDot( e.originalEvent.touches[0].clientX, e.originalEvent.touches[0].clientY  );
+  function init() {
+    const imageEditor = document.getElementById('image-editor');
+    const ctx = imageEditor.getContext('2d');
+
+    imageEditor.addEventListener('touchstart', (e) => {
+      console.log(e);
+      paintDot(e.touches[0].clientX, e.touches[0].clientY);
     });
 
-    var ctx =  $( '#imagefield' ).get(0).getContext( '2d' );
-
-    var paintDot = function(x,y) {
-        ctx.fillStyle = 'red';
-        ctx.beginPath(); // sonst wird alles zu einer Form und mit Farbe gefüllt
-        ctx.arc( x, y, 10, 0, 2*Math.PI );
-        ctx.fill();
+    function paintDot(x, y) {
+      ctx.fillStyle = 'red';
+      ctx.beginPath();
+      ctx.arc(x, y, 10, 0, 2 * Math.PI);
+      ctx.fill();
     }
-
-  
-
-
-
-}
-
-
-if ( APP ) {
-    document.addEventListener( 'deviceready', starteApp );
-} else {
-    // zum Testen ohne PhoneGap
-    starteApp();
-}
-
-
-
+  }
+})();
