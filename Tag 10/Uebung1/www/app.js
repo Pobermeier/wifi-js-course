0cd822f5a5529f4ed15c15414dcbd042244139ff
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-06-30 11:27:44 +0200

    UPDATE Day 10 - Exercise 1

--- Tag 10/Uebung1/www/app.js
+++ Tag 10/Uebung1/www/app.js
@@ -1,4 +1,9 @@
 (function () {
+  const state = {
+    currentColor: 0,
+    availColors: ['red', 'green', 'blue', 'yellow'],
+  };
+
   if (APP) {
     document.addEventListener('deviceready', init);
   } else {
@@ -8,14 +13,30 @@
   function init() {
     const imageEditor = document.getElementById('image-editor');
     const ctx = imageEditor.getContext('2d');
+    const selectedColor = document.getElementById('current-color');
+
+    selectedColor.style.backgroundColor = state.availColors[state.currentColor];
+
+    selectedColor.addEventListener('click', () => {
+      const nextColor =
+        state.currentColor < state.availColors.length - 1
+          ? state.availColors[++state.currentColor]
+          : resetColor();
+      selectedColor.style.backgroundColor = nextColor;
+    });
 
     imageEditor.addEventListener('touchstart', (e) => {
       console.log(e);
       paintDot(e.touches[0].clientX, e.touches[0].clientY);
     });
 
+    function resetColor() {
+      state.currentColor = 0;
+      return state.availColors[state.currentColor];
+    }
+
     function paintDot(x, y) {
-      ctx.fillStyle = 'red';
+      ctx.fillStyle = state.availColors[state.currentColor];
       ctx.beginPath();
       ctx.arc(x, y, 10, 0, 2 * Math.PI);
       ctx.fill();
